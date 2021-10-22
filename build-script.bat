c07157c83926b2f94e7eedae541c96954daf6787
ivwall <ivwall@tgndomains.com>
2021-10-22 06:25:53 -0700

    mvn build-script

---
+++ build-script.bat
@@ -0,0 +1,11 @@
+rem mvn archetype:generate 
+rem    -DarchetypeGroupId=io.cucumber 
+rem    -DarchetypeArtifactId=cucumber-archetype 
+rem    -DarchetypeVersion=6.10.4 
+rem    -DgroupId=webkernel 
+rem    -DartifactId=webkernel 
+rem    -Dpackage=webkernel 
+rem    -Dversion=1.0.0-SNAPSHOT 
+rem    -DinteractiveMode=false
+
+mvn archetype:generate -DarchetypeGroupId=io.cucumber -DarchetypeArtifactId=cucumber-archetype -DarchetypeVersion=6.10.4 -DgroupId=webkernel -DartifactId=webkernel -Dpackage=webkernel -Dversion=1.0.0-SNAPSHOT -DinteractiveMode=false
