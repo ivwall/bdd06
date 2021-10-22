b7e475e93eb1ef4ba65760ec6084234522cc55fd
ivwall <ivwall@tgndomains.com>
2021-10-22 06:30:51 -0700

    renamed project to anw api n web kernel

--- build-script.bat
+++ build-script.bat
@@ -1,11 +1,14 @@
+rem anw stands for API aNd Web 
+rem a cucumber research effort
+
 rem mvn archetype:generate 
 rem    -DarchetypeGroupId=io.cucumber 
 rem    -DarchetypeArtifactId=cucumber-archetype 
 rem    -DarchetypeVersion=6.10.4 
-rem    -DgroupId=webkernel 
-rem    -DartifactId=webkernel 
-rem    -Dpackage=webkernel 
+rem    -DgroupId=anwkernel 
+rem    -DartifactId=anwkernel 
+rem    -Dpackage=anwkernel 
 rem    -Dversion=1.0.0-SNAPSHOT 
 rem    -DinteractiveMode=false
 
-mvn archetype:generate -DarchetypeGroupId=io.cucumber -DarchetypeArtifactId=cucumber-archetype -DarchetypeVersion=6.10.4 -DgroupId=webkernel -DartifactId=webkernel -Dpackage=webkernel -Dversion=1.0.0-SNAPSHOT -DinteractiveMode=false
+mvn archetype:generate -DarchetypeGroupId=io.cucumber -DarchetypeArtifactId=cucumber-archetype -DarchetypeVersion=6.10.4 -DgroupId=anwkernel -DartifactId=anwkernel -Dpackage=anwkernel -Dversion=1.0.0-SNAPSHOT -DinteractiveMode=false
